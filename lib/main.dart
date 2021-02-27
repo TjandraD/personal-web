a6b64b143a183b8be804e0883043291abf597faa
TjandraD <t.darmo.t@gmail.com>
2021-02-27 19:38:20 +0700

    style: add default constants & textstyle

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio_web/common/styles.dart';
 import 'package:url_strategy/url_strategy.dart';
 
 void main() {
@@ -10,7 +11,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(textTheme: textTheme),
       title: 'Tjandra Darmo',
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
+      ),
     );
   }
 }
