a78ca302e8628d8e97289ec224bf1fac754b44e5
TjandraD <t.darmo.t@gmail.com>
2021-02-27 19:28:16 +0700

    feat: remove # in web url

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
+  setPathUrlStrategy();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: 'Tjandra Darmo',
+    );
   }
 }
